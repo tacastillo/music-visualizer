a890e0924d8c428a0e4e7d0772803ccabab964f9
Timothy Castillo <tacastillo@usfca.edu>
2016-03-22 15:23:32 -0700

    updating for gh-pages

---
+++ player.js
@@ -0,0 +1,13 @@
+// var client_id = "3be5a9d1c00a384df4367a567184b0f9";
+
+// SC.initialize({
+//   client_id: client_id,
+// });
+
+// var flicker = "160632928",
+// 	spazzkid = "192143988",
+// 	pystrance = "235895843"
+
+// SC.stream('/tracks/' + spazzkid).then(function(player){
+//   player.play();
+// });
--- visualization.js
+++ visualization.js
@@ -1,18 +1,24 @@
-var api_key = "&api_key=dc6zaTOxFJmzC";
-var base_url = "http://api.giphy.com/v1/gifs/search?q=";
-var query = "80s_cartoons"
+var colorTime = 500,
+	gifTime = 2000,
+	opacity = 0.6,
+	numGifs = 100,
+	width = $(document.body).width(),
+	height = $(document.body).height();
+
+var query = "drugs",
+	api_key = "dc6zaTOxFJmzC",
+	params = "&api_key=" + api_key + "&limit=" + numGifs,
+	base_url = "http://api.giphy.com/v1/gifs/search?q=";
 
 var backgrounds;
-var resultGet = $.get(base_url + query + api_key, function(result) {
-	backgrounds = result;
-	var index = Math.floor((Math.random() * 24) + 1);
-	var gifURL = "url(" + backgrounds["data"][index]["images"]["original"]["url"] + ")";
+var resultGet = $.get(base_url + query + params, function(result) {
+	console.log(query + ": " + result.data.length)
+	backgrounds = result.data;
+	var index = Math.floor(Math.random() * backgrounds.length-1);
+	var gifURL = "url(" + backgrounds[index]["images"]["original"]["url"] + ")";
 	$(document.body).css('background-image', gifURL);
-	setTimeout(changeBackground(), 2000);
+	setTimeout(changeBackground(), gifTime);
 });
-
-var height = $(document.body).height();
-var width = $(document.body).width();
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -21,7 +27,7 @@ var svg = d3.select("body").append("svg")
 svg.append('rect')
 	.attr('width', width)
 	.attr('height', height)
-	.style('opacity', 0.6);
+	.style('opacity', opacity);
 
 var rect = d3.select("body").select("svg").select("rect");
 changeFilter();
@@ -29,13 +35,12 @@ changeFilter();
 function changeFilter() {
 	var color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
 	rect.style('fill', color);
-	setTimeout(changeFilter, 500);
+	setTimeout(changeFilter, colorTime);
 }
 
 function changeBackground() {
-	index = Math.floor((Math.random() * 24) + 1);
-	console.log(index);
-	gifURL = "url(" + backgrounds["data"][index]["images"]["original"]["url"] + ")";
+	var index = Math.floor(Math.random() * backgrounds.length);
+	gifURL = "url(" + backgrounds[index]["images"]["original"]["url"] + ")";
 	$(document.body).css('background-image', gifURL);
-	setTimeout(changeBackground, 1500);
+	setTimeout(changeBackground, gifTime);
 }
